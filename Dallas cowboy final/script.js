5b7c442fd516ad444fa73f05bd9dd6bff2d0fdaa
Dpeters17 <Danielpeters596@yahoo.com>
2026-05-17 12:20:17 -0500

    Add files via upload

---
+++ Dallas cowboy final/script.js
@@ -0,0 +1,16 @@
+function showMessage() {
+    alert("Thank you for visiting Explore the Stadium!");
+}
+
+
+function showMessage() {
+    alert("Thank you for visiting the Dallas Cowboys website!");
+}
+
+
+
+function changeTheme() {
+
+    document.body.style.backgroundColor = "#d6dce5";
+
+}
